69739623f0655c8178c2807ab81be77103fb3ba2
Jussi Rajaniemi <jrajaniemi@mc-lifestyle.com>
2024-07-19 18:54:34 +0300

    Initial Commit

---
+++ BikeComputer/BikeComputerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeComputerApp.swift
+//  BikeComputer
+//
+//  Created by Jussi Rajaniemi on 19.7.2024.
+//
+
+import SwiftUI
+
+@main
+struct BikeComputerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BikeComputerUITests/BikeComputerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BikeComputerUITestsLaunchTests.swift
+//  BikeComputerUITests
+//
+//  Created by Jussi Rajaniemi on 19.7.2024.
+//
+
+import XCTest
+
+final class BikeComputerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
