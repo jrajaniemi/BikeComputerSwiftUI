b55e588283585fa98cba8435f1917949c30e704b
Jussi Rajaniemi <jussi.rajaniemi@alfame.com>
2024-07-20 22:08:43 +0300

    Updated interface

--- BikeComputer/ContentView.swift
+++ BikeComputer/ContentView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Spacer()
+        GeometryReader { geometry in
+            VStack(spacing: 0) {
+                Text("A")
+                    .font(.custom("CustomFontName", size: 24))
+                    .frame(width: geometry.size.width, height: geometry.size.height * 6 / 12)
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                
+                HStack(spacing: 0) {
+                    Text("B")
+                        .font(.custom("CustomFontName", size: 24))
+                        .frame(width: geometry.size.width / 2, height: geometry.size.height * 3 / 12)
+                        .background(Color.yellow)
+                        .foregroundColor(.black)
+                    Text("C")
+                        .font(.custom("CustomFontName", size: 24))
+                        .frame(width: geometry.size.width / 2, height: geometry.size.height * 3 / 12)
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                }
+                
+                Text("D")
+                    .font(.custom("CustomFontName", size: 24))
+                    .frame(width: geometry.size.width, height: geometry.size.height * 3 / 12)
+                    .background(Color.black)
+                    .foregroundColor(.white)
+            }
+            .edgesIgnoringSafeArea(.all)
         }
-        .padding()
+
     }
 }
 
