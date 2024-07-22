7ed4941ee103402a1a0148d1f97be5bc0af6cb0e
Jussi Rajaniemi <jussi.rajaniemi@alfame.com>
2024-07-22 17:02:19 +0300

    update

---
+++ BikeComputer/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  BikeComputer
+//
+//  Created by Jussi Rajaniemi on 21.7.2024.
+//
+
+import Foundation
---
+++ BikeComputer/RouteManager.swift
@@ -0,0 +1,8 @@
+//
+//  RouteManager.swift
+//  BikeComputer
+//
+//  Created by Jussi Rajaniemi on 22.7.2024.
+//
+
+import Foundation
